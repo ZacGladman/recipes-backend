aca006b5f3cb8984f06a31e667a5dfd5bb73a4b2
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-17 16:44:02 +0000

    edit recipe_reviews sql file

--- sql/RecipeReviews.sql
+++ sql/RecipeReviews.sql
@@ -1,12 +1,10 @@
-DROP TABLE IF EXISTS recipe_reviews;
-
 CREATE TABLE  recipe_reviews (
     review_id          serial PRIMARY KEY,
-    recipe_id          integer,
+    recipe_api_id          integer,
     user_id            integer,
     rating_value       decimal,
     review              text,
-    FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id),
+    submission_time     date,
     FOREIGN KEY(user_id) REFERENCES users(user_id),
-    UNIQUE(recipe_id, user_id)
+    UNIQUE(recipe_api_id, user_id)
 );
