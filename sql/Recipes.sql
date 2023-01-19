01b8df16d530c55026b81cb0e66457dfca1880d9
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-19 16:36:17 +0000

    update Recipes.sql

--- sql/Recipes.sql
+++ sql/Recipes.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS recipes;
 
 CREATE TABLE  recipes (
     recipe_id          serial PRIMARY KEY,
-    recipe_API_id        varchar(255) NOT NULL,
-    recipe_name           varchar(255) NOT NULL UNIQUE
+    recipe_api_id        varchar(255) NOT NULL,
+    recipe_name           varchar(255) NOT NULL UNIQUE,
+    recipe_img_url        text,
+    UNIQUE(recipe_api_id)
 );
