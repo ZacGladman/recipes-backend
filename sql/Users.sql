80eef38d1f3ad6937955dbc32863eca04e0d483a
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-19 22:26:45 +0000

    edit Users.sql

--- sql/Users.sql
+++ sql/Users.sql
@@ -3,7 +3,8 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE  users (
     user_id          serial PRIMARY KEY,
     username        varchar(255) NOT NULL,
-    email           varchar(255) NOT NULL UNIQUE
+    email           varchar(255) NOT NULL UNIQUE,
+    profile_pic     text
 );
 
 INSERT INTO users (username, email) VALUES ('zac', 'zacg@email')
