511fa44e9d5f81b2aa57712bd658a73bf4047f96
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-19 16:35:47 +0000

    update CookList.sql

--- sql/CookList.sql
+++ sql/CookList.sql
@@ -1,7 +1,10 @@
+DROP TABLE IF EXISTS cooklist;
+
 CREATE TABLE  cooklist (
     cooklist_id          serial PRIMARY KEY,
-    recipe_api_id          integer,
+    recipe_id          integer,
     user_id            integer,
     FOREIGN KEY(user_id) REFERENCES users(user_id),
-    UNIQUE(recipe_api_id, user_id)
+    FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id),
+    UNIQUE(recipe_id, user_id)
 );
