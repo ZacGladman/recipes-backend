bcfb16b13e0db87d0edf8dae1a0adab5aa9f75a4
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-21 19:10:01 +0000

    update RecipeReviews.sql

--- sql/RecipeReviews.sql
+++ sql/RecipeReviews.sql
@@ -1,11 +1,11 @@
 CREATE TABLE  recipe_reviews (
     review_id          serial PRIMARY KEY,
-    recipe_id          integer,
+    recipe_api_id          varchar(255),
     user_id            integer,
     rating_value       decimal,
     review              text,
     submission_time     date,
     FOREIGN KEY(user_id) REFERENCES users(user_id),
-    FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id),
-    UNIQUE(recipe_id, user_id)
+    FOREIGN KEY(recipe_api_id) REFERENCES recipes(recipe_api_id),
+    UNIQUE(recipe_api_id, user_id)
 );
